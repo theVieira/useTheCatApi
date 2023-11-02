85260c74aa60a62baab71c79664df0710a3bafc4
Gabriel <gabrielvieiraguimaraes4@gmail.com>
2023-11-02 08:46:04 -0400

    Js Changed

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
-//Set Id
-const div = document.querySelectorAll('.img')
-
-div.forEach((element, id = 0) => {
-  element.id = 'img' + id
-  id++
-})
-
 //Api
 const key = 'live_0wWnMe92OC21zZzBvoKUypToQuBT2oqO5qdGcvqDDksOfCVWJNca4mbxvzZRGOSN'
-
 //https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=&${key}
-
-document.querySelector('#search').addEventListener('click', () => apiReq())
 
 async function apiReq() {
   const data = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=&${key}`)
   .then((res => res.json())
   )
 
+  showImg(data)
+}
+
+function showImg(data) {
   for(let i = 0; i < 4; i++) {
     const container = document.querySelector(`#img${i}`)
     container.setAttribute('style', `background:url(${data[i].url}); background-repeat:no-repeat; background-position:center; background-size:cover`)
@@ -25,7 +18,4 @@ async function apiReq() {
   }
 }
 
-function showImg(url) {
-  const imgContainer = document.getElementById('1')
-  imgContainer.setAttribute('style', `background:url(${url}); background-repeat:no-repeat; background-position:center`)
-}
+document.querySelector('#search').addEventListener('click', () => apiReq())
